8abc93c791d7a9f41a67dcf8050490bcd6f9d5e6
Daniil Dubovik <daniil.dubovik@gmail.com>
2023-10-11 19:15:02 +0300

    Added `IView` and `IModel` interfaces

--- Assets/MVC/Controller.cs
+++ Assets/MVC/Controller.cs
@@ -1,6 +1,8 @@
 ﻿namespace Mvc
 {
     public class Controller<TView, TModel> : IController
+        where TView : IView
+        where TModel : IModel
     {
         protected TView view;
         protected TModel model;
